42d9f26d56959d8c16cc3eaf6f6d9c72bd15f55f
Brian Ketelsen <bketelsen@gmail.com>
2016-02-19 19:19:52 -0500

    model tests

--- example/design/models.go
+++ example/design/models.go
@@ -10,10 +10,12 @@ var _ = StorageGroup("GoaCellar", func() {
 	Store("postgres", gorma.Postgres, func() {
 		Description("This is the Postgres relational store")
 		Model("Account", func() {
-
+			RendersTo(Account)
+			HasMany("Bottles", "Bottle")
 		})
 		Model("Bottle", func() {
-
+			RendersTo(Bottle)
+			BelongsTo("Account")
 		})
 	})
 })
